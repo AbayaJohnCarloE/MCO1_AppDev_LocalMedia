8a5c0dec0addaefb7d4e4c397061e509a1432de5
AbayaJohnCarloE <146050272+AbayaJohnCarloE@users.noreply.github.com>
2024-04-25 02:14:57 +0800

    Add files via upload
    members:
    Abaya John Carlo
    Quitasol Kenlie

---
+++ MCO_AppDev/H.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Find the Log Out button by its id
+    var logoutBtn = document.getElementById("logoutBtn");
+    
+    // Add click event listener to the Log Out button
+    logoutBtn.addEventListener("click", function() {
+      // Perform logout action here, such as redirecting to the login page
+      window.location.href = "ph.html";
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Find the Log Out button by its id
+    var logoutBtn = document.getElementById("logoutBtn");
+    
+    // Add click event listener to the Log Out button
+    logoutBtn.addEventListener("click", function() {
+      // Perform logout action here, such as redirecting to the login page
+      window.location.href = "SU.html";
+    });
+  });
+  
